223b96bd3428c108ab5e7fbc9c1ca3e55f150c85
CaoPhiHung <caophihung8392@gmail.com>
2018-11-09 16:17:02 -0800

    Update src/main/java/app/ClientMain.java

--- src/main/java/app/ClientMain.java
+++ src/main/java/app/ClientMain.java
@@ -1,28 +1,9 @@
 package main.java.app;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.Date;
-
-import main.java.model.EventAction;
-import main.java.model.GenericDLinkedList;
-import main.java.model.Goods;
-import main.java.model.Order;
-import main.java.model.OrderDetail;
-import main.java.model.User;
-
 import main.java.client.ClientFrameMain;
-import main.java.service.ClientConnectService;
 
 public class ClientMain {
 
-	private Socket socket;
-	private ObjectInputStream ois;
-	private ObjectOutputStream oos;
-
-	
     public static void main(String[] args) throws IOException {
         // Run client frame
         ClientFrameMain cfm = new ClientFrameMain();
